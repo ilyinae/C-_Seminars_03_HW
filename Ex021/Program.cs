e14a487081e29d4767d38c34c2ec691bf4cca2b0
Anton Ilyin <ilyin.a.e@gmail.com>
2022-09-24 11:31:54 +0300

    Added Ex021

---
+++ Ex021/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите координату Х1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату Х2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Расстояние между точками: " + Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2)+ Math.Pow(z2-z1, 2)));
+
