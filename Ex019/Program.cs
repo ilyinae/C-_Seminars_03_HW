186f249d94ffb115d2066163c591646a55d693b4
Anton Ilyin <ilyin.a.e@gmail.com>
2022-09-24 11:26:36 +0300

    Added Ex019

---
+++ Ex019/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите пятизначное число: ");
+string num = Console.ReadLine();
+if (num.Length == 5)
+    Console.WriteLine(num[0] == num[4] && num[1] == num[3]);
+else    
+    Console.WriteLine("Ошибка ввода");
